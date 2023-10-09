6fb5e7e2f64fddd2700d1edc73033e859b76d7f1
TheBlackFern <97515538+TheBlackFern@users.noreply.github.com>
2023-10-09 21:56:15 +0300

    🩹 actually include favicons

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -31,6 +31,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
       <body
         className={cn(
           inter.className,
--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -26,7 +26,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <Providers> */}
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
       <body className={inter.className}>
         <Providers>
           <MainNav />
@@ -43,7 +60,6 @@ export default function RootLayout({
           <MobileNav />
         </Providers>
       </body>
-      {/* </Providers> */}
     </html>
   );
 }
